b15ae4f367f2989203ba832316c7fe94c1c8ac70
podpage <support@podpage.org>
2017-01-16 23:01:05 +0100

    Removed unused code

--- src/main/java/org/podpage/alexa/skills/defaultskills/DemoSkill.java
+++ src/main/java/org/podpage/alexa/skills/defaultskills/DemoSkill.java
@@ -12,9 +12,6 @@ public class DemoSkill implements Skill {
         ResponseContent rc = new ResponseContent();
         rc.addContent("{\"version\": \"1.0\", \"response\": { \"outputSpeech\": { \"type\": \"SSML\", \"ssml\": \"<speak>Im Moment sind " + new Random().nextInt(10) + " User auf dem Server</speak>\" }, \"reprompt\": { \"outputSpeech\": { \"type\": \"PlainText\", \"text\": \"Was willst du denn wissen?\" } }, \"shouldEndSession\": true } }");
         ResponseHeader responseHeader = new ResponseHeader(ResponseHeader.ResponseCode.CODE_200);
-        responseHeader.getFields().clear();
-        responseHeader.addHeaderField(Header.HeaderField.CONTENT_TYPE.setContent("application/json"));
-        responseHeader.addHeaderField(Header.HeaderField.SERVER.setContent("AlexaServer by @podpage"));
         return new SkillResponse(responseHeader, rc);
     }
 }
